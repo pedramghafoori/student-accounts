67152a14d5fb87203eedbe2187a5fb0e27477a04
pedramghafoori <pedramghafoori@hotmail.com>
2025-04-07 15:07:20 -0400

    Enhance ReschedulePage: add dynamic export for improved rendering behavior

--- app/reschedule/page.js
+++ app/reschedule/page.js
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import React, { useEffect, useState, useMemo } from "react";
 import { useSearchParams, useRouter } from "next/navigation";
 import axios from "axios";
